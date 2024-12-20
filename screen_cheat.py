dfd1273da01e1c71813b2e15359040838169dcca
Dark Mode <darkkmodee00@gmail.com>
2024-12-20 16:19:36 -0500

    Lying to my friends

--- screen_cheat.py
+++ screen_cheat.py
@@ -1,10 +1,18 @@
-import math
-
+import random
 import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import numpy as np
+
+# Whether to show real or fake numbers
+LIE_TO_FRIENDS = True
+SHOW_TEXT = "Eye, Heart, Temp Stimulation Tracker"
+MIN_BODY_TEMP = random.uniform(96.0, 97.0)
+MAX_BODY_TEMP = MIN_BODY_TEMP + random.uniform(1, 2)
+
+MIN_HEARTRATE = random.randint(63, 87)
+MAX_HEARTRATE = MIN_HEARTRATE + random.randint(4, 7)
 
 # Mediapipe constants
 LEFT_EYE_INNER_INDEX = 133
@@ -45,7 +53,7 @@ def calibration_step():
     # Get the FPS of the video stream
     fps = cap.get(cv2.CAP_PROP_FPS)
 
-    print(f"Calibration started. Look at the center of the screen and hit enter.")
+    print(f"Pupil size & movement calibration started. Look at center of target area.")
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -61,7 +69,8 @@ def calibration_step():
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
         detection_result = face_landmarker.detect(mp_image)
 
-        visualize_landmarks(detection_result, frame)
+        if not LIE_TO_FRIENDS:
+            visualize_landmarks(detection_result, frame)
 
         if detection_result.face_landmarks:
             # Process the first detected face
@@ -98,12 +107,13 @@ def calibration_step():
                         "pitch": pitch
                     }
 
-                    print(face_calibration)
+                    if not LIE_TO_FRIENDS:
+                        print(face_calibration)
 
                     return face_calibration
 
         # Display the full-screen video
-        cv2.imshow("Simplified Gaze Tracking", frame)
+        cv2.imshow(f"{SHOW_TEXT}", frame)
 
         # Exit when 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -136,19 +146,20 @@ def adjust_distance(pupil_to_nose_distance: float, current_pitch: float, calibra
     return adjusted_distance
 
 
-def main(calibration):
+def main(calibration, user_screen):
     num_frames_looking_each_direction = {
         "Up": 0,
         "Down": 0,
         "Straight": 0
     }
+    current_heartrate = 75
+    current_body_temp = 98
+    current_eye_dilation = 'medium'
 
     cap = cv2.VideoCapture(0)
 
     # Get the FPS of the video stream
     fps = cap.get(cv2.CAP_PROP_FPS)
-
-    print(f"Frames per second: {fps}")
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -164,7 +175,8 @@ def main(calibration):
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
         detection_result = face_landmarker.detect(mp_image)
 
-        visualize_landmarks(detection_result, frame)
+        if not LIE_TO_FRIENDS:
+            visualize_landmarks(detection_result, frame)
 
         if detection_result.face_landmarks:
             # Process the first detected face
@@ -184,25 +196,32 @@ def main(calibration):
                 gaze_text = "Down"
                 num_frames_looking_each_direction['Down'] += 1
 
-            # Display status text
-            cv2.putText(frame, gaze_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            current_body_temp, current_eye_dilation, current_heartrate = generate_fake_numbers(current_body_temp, current_heartrate, current_eye_dilation)
 
-            # Display num frames
-            cv2.putText(frame, f"Straight: {round(num_frames_looking_each_direction['Straight'] / 30, 1)}s", (150, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-            cv2.putText(frame, f"Up: {round(num_frames_looking_each_direction['Up'] / 30, 1)}s", (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-            cv2.putText(frame, f"Down: {round(num_frames_looking_each_direction['Down'] / 30, 1)}s", (250, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            if LIE_TO_FRIENDS:
+                cv2.putText(frame, f"Body Temp: {current_body_temp}", (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+                cv2.putText(frame, f"Heart Rate: {current_heartrate}", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+                cv2.putText(frame, f"Pupil Dilation: {current_eye_dilation}", (0, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
+            if not LIE_TO_FRIENDS:
+                cv2.putText(frame, f"Straight: {round(num_frames_looking_each_direction['Straight'] / 30, 1)}s", (150, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+                cv2.putText(frame, f"Up: {round(num_frames_looking_each_direction['Up'] / 30, 1)}s", (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+                cv2.putText(frame, f"Down: {round(num_frames_looking_each_direction['Down'] / 30, 1)}s", (250, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
         # Display the full-screen video
-        cv2.imshow("Simplified Gaze Tracking", frame)
+        cv2.imshow(f"{SHOW_TEXT}", frame)
 
         # Exit when 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     # Print the results
-    print(f"Straight: {round(num_frames_looking_each_direction['Straight'] / 30, 1)}s")
-    print(f"Up: {round(num_frames_looking_each_direction['Up'] / 30, 1)}s")
-    print(f"Down: {round(num_frames_looking_each_direction['Down'] / 30, 1)}s")
+    top_text = 'NP Value' if user_screen == 'top' else 'P Value'
+    bottom_text = 'NP Value' if user_screen == 'bottom' else 'P Value'
+
+    print(f"S: {round(num_frames_looking_each_direction['Straight'] / 30, 1)}s")
+    print(f"{top_text}: {round(num_frames_looking_each_direction['Up'] / 30, 1)}s")
+    print(f"{bottom_text}: {round(num_frames_looking_each_direction['Down'] / 30, 1)}s")
 
     cap.release()
     cv2.destroyAllWindows()
@@ -247,9 +266,10 @@ def determine_gaze(face_landmark, calibration, frame):
     total_diff = round(current_diff - calib_diff, decimal_places_rounding)
 
     # Print current total difference
-    cv2.putText(frame, f"current pitch: {current_pitch}", (100, 100),
-                cv2.FONT_HERSHEY_SIMPLEX, 1,
-                (0, 0, 0), 2)
+    if not LIE_TO_FRIENDS:
+        cv2.putText(frame, f"current pitch: {current_pitch}", (100, 100),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (0, 0, 0), 2)
 
     # Determine gaze direction
     if total_diff > 0.0004:
@@ -258,6 +278,32 @@ def determine_gaze(face_landmark, calibration, frame):
         return 1
     else:
         return 0
+
+
+def generate_fake_numbers(current_body_temp: float, current_heartrate: int, current_eye_dilation: str):
+    # Body temp
+    if random.random() < 0.05:
+        body_temp = round(
+            max(MIN_BODY_TEMP, min(MAX_BODY_TEMP, current_body_temp + random.uniform(-0.1, 0.1))), 1
+        )
+    else:
+        body_temp = current_body_temp
+
+    # Eye dilation
+    if random.random() < 0.005:
+        possible_dilations = ['medium', 'high', 'low']
+        possible_dilations.remove(current_eye_dilation)  # Exclude the current state
+        eye_dilation = random.choice(possible_dilations)  # Pick a new state
+    else:
+        eye_dilation = current_eye_dilation
+
+    # Heart rate
+    if random.random() < 0.03:
+        heartrate = max(MIN_HEARTRATE, min(MAX_HEARTRATE, current_heartrate + random.randint(-1, 1)))
+    else:
+        heartrate = current_heartrate
+
+    return body_temp, eye_dilation, heartrate
 
 
 def visualize_landmarks(detection_result, frame):
@@ -272,5 +318,15 @@ def visualize_landmarks(detection_result, frame):
 
 
 if __name__ == "__main__":
+    screen = 'top'
+
+    if not LIE_TO_FRIENDS:
+        user_input = input("Top Screen? (y/n):")
+    else:
+        user_input = input("Press a button to Start:")
+
+    if user_input != 'y':
+        screen = 'bottom'
+
     calibration_results = calibration_step()
-    main(calibration_results)
+    main(calibration_results, screen)
